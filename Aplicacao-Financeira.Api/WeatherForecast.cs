00598622f415833d83c554352564a307b7c29616
anagiovanni <anagiovanni66@gmail.com>
2025-08-06 10:52:11 -0300

    Iniciado a API

---
+++ Aplicacao-Financeira.Api/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Aplicacao_Financeira.Api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
