66a11d1bda05ca76d11b7197487126c59165c4b6
anagiovanni <anagiovanni66@gmail.com>
2025-08-27 10:47:55 -0300

    Configurando Serviços

--- Aplicacao-Financeira.Api/Data/AppDbContext.cs
+++ Aplicacao-Financeira.Api/Data/AppDbContext.cs
@@ -25,18 +25,18 @@ namespace Aplicacao_Financeira.Api.Data
         {
             #region Populate Roles - Perfis de Usuário
             List<IdentityRole> roles = new()
-        {
+            {
             new IdentityRole() {
                Id = "0b44ca04-f6b0-4a8f-a953-1f2330d30894",
                Name = "Administrador",
                NormalizedName = "ADMINISTRADOR"
-            },
+                },
             new IdentityRole() {
                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
                Name = "Usuário",
                NormalizedName = "USUÁRIO"
-            },
-        };
+                },
+            };
             builder.Entity<IdentityRole>().HasData(roles);
             #endregion
 
@@ -44,17 +44,17 @@ namespace Aplicacao_Financeira.Api.Data
             List<User> usuarios = new() {
             new User(){
                 Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
-                Email = "gallojunior@gmail.com",
-                NormalizedEmail = "GALLOJUNIOR@GMAIL.COM",
-                UserName = "GalloJunior",
-                NormalizedUserName = "GALLOJUNIOR",
+                Email = "giovanninicolau666@gmail.com",
+                NormalizedEmail = "GIOVANNINICOLAU666@GMAIL.COM",
+                UserName = "GiLau",
+                NormalizedUserName = "GILAU",
                 LockoutEnabled = true,
                 EmailConfirmed = true,
-                Name = "José Antonio Gallo Junior",
+                Name = "Nicolau Carneiro Pinto Junior",
                 // DataNascimento = DateTime.Parse("05/08/1981"),
                 // Foto = "/img/usuarios/ddf093a6-6cb5-4ff7-9a64-83da34aee005.png"
             }
-        };
+            };
             foreach (var user in usuarios)
             {
                 PasswordHasher<IdentityUser> pass = new();
@@ -65,20 +65,16 @@ namespace Aplicacao_Financeira.Api.Data
 
             #region Populate UserRole - Usuário com Perfil
             List<IdentityUserRole<string>> userRoles = new()
-        {
+            {
             new IdentityUserRole<string>() {
                 UserId = usuarios[0].Id,
                 RoleId = roles[0].Id
-            },
+                },
             new IdentityUserRole<string>() {
                 UserId = usuarios[0].Id,
                 RoleId = roles[1].Id
-            },
-            new IdentityUserRole<string>() {
-                UserId = usuarios[0].Id,
-                RoleId = roles[2].Id
-            }
-        };
+                },
+            };
             builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
             #endregion
         }
